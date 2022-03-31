690f6b46ba85cc74f3e9ac47fa6fe1e262901f94
rodriggoshell <rodriggoshell@gmail.com>
2022-03-30 22:31:58 -0300

    Resolvido o Bug do carai

--- server.py
+++ server.py
@@ -4,16 +4,14 @@ from src.schemes.schemes import Produto
 from src.infra.sqlalchemy.config.database import get_db, criar_db
 from src.repositorios.produto import RepositorioProduto
 
+criar_db()
 
 app = FastAPI()
 
-
-criar_db()
-
 @app.post('/produtos')
 def criar_produtos(produto: Produto, db: Session = Depends(get_db)):
-    produto_criado = RepositorioProduto().criar(produto)
-    return produto_criado 
+    produtos_criado = RepositorioProduto(db).criar(produto)
+    return produtos_criado 
 
 @app.get('/produtos')
 def listar_produtos():
--- src/infra/sqlalchemy/models/models.py
+++ src/infra/sqlalchemy/models/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Float, Boolean
 from src.infra.sqlalchemy.config.database import Base
 
-class Produtos(Base): 
+class Produto(Base): 
     __tablename__ = 'produto'
     id = Column(Integer, primary_key=True, index=True)
     nome = Column(String)
