efff5a18aad03ed5a92811de7e3b4e033c04516b
rodriggoshell <rodriggoshell@gmail.com>
2022-03-30 22:01:37 -0300

    Resolvido o Bug do carai

--- server.py
+++ server.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 from src.schemes.schemes import Produto
-from src.infra.sqlalchemy.repositorios.produto import RepositorioProduto
+from src.infra.sqlalchemy.config.database import get_db, criar_db
+from src.repositorios.produto import RepositorioProduto
+
 
 app = FastAPI()
 
+
+criar_db()
+
 @app.post('/produtos')
-def criar_produtos(produto: Produto):
-    return f'Produto cadastrado'
+def criar_produtos(produto: Produto, db: Session = Depends(get_db)):
+    produto_criado = RepositorioProduto().criar(produto)
+    return produto_criado 
 
 @app.get('/produtos')
 def listar_produtos():
